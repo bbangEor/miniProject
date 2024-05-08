a80aa706de45f6a83cfe29aaa8006df87a245256
ning00060 <ning00060@gmail.com>
2024-05-08 17:07:52 +0900

    main1

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,10 +1,55 @@
 package test;
 
-public class MainTest {
+import java.awt.Component;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import components.Player;
+import components.Score;
+
+public class MainTest extends JFrame {
+	private Player player;
+	private Score score;
+	private JLabel backgroundMap;
+
+	public MainTest() {
+		initData();
+		setInitLayout();
+
+	}
+
+	public void initData() {
+		backgroundMap = new JLabel(new ImageIcon("img.jpg"));
+		player = new Player();
+	}
+
+	public void setInitLayout() {
+		setLayout(null);
+		setVisible(true);
+
+	}
+
+	public void addEventListener() {
+		this.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+		});
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MainTest();
 	}
 
 }
